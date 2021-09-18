5a85646d40cac6268b5115b1454b77360fb7c8d9
Berk Burak Tasdemir <burakt546@live.com>
2021-09-18 03:15:40 -0700

    Initial commit

---
+++ tood-app/tood-appTests/tood_appTests.swift
@@ -0,0 +1,33 @@
+//
+//  tood_appTests.swift
+//  tood-appTests
+//
+//  Created by Berk Burak Tasdemir on 9/18/21.
+//
+
+import XCTest
+@testable import tood_app
+
+class tood_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
